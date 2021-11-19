c91d5777409129482802068335bbab205479586a
jharashchuk <j.garaschuk@gmail.com>
2021-11-19 20:44:41 +0100

    add task1

---
+++ script/task1.js
@@ -0,0 +1,42 @@
+// Задание 1! Поиск данных в массиве.
+// Создадим массив объектов, который будет содержать данные о пользователях;
+// Выведем окно prompt, в которое необходимо будет ввести фамилию пользователя;
+// Написать инструкцию поиска по массиву data, с введенными в prompt данными;
+// Если есть совпадение - вывести все данные этого пользователя в altert;прим:
+// user name: Ashton Kutcher.
+// user age: 40.
+// Если совпадения не произошло вывести alert с ошибкой: 
+// “No results found for your request”
+// Добавить в функционал возможность поиска пользователя без учета регистра.
+
+let data = [
+    {
+        firstName: 'Ashton',
+        lastName: 'Kutcher',
+        age: 40
+    }, {
+        firstName: 'Bradley',
+        lastName: 'Pitt',
+        age: 54
+    }, {
+        firstName: 'Hannah',
+        lastName: 'Dakota',
+        age: 24
+    }
+ ];
+
+let surname = prompt('Введите вашу фамилию', '');
+let info;
+
+for (let elem of data) {
+    if(elem.lastName == surname) {
+        info = elem;
+    }
+}
+
+if(info) {
+    alert(`user name: ${info.firstName} ${info.lastName}
+           user age: ${info.age}`);    
+} else {
+    alert ('No results found for your request');
+}
